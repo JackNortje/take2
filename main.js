89a7ad53d4020ebad54d8f4a9d7dbb78ed201e48
Jack Nortje <jacknortje24@gmail.com>
2021-10-01 10:43:13 +0200

    Nearly complete

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const faders = document.querySelectorAll('.animate__fadeIn')
+
+
 function workPlace(evt, industryName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -12,3 +15,22 @@ function workPlace(evt, industryName) {
     evt.currentTarget.className += " active";
   }
 
+  const appearOptions = {
+    threshold: 0,
+    rootMargin: '0px 0px -100px 0px'
+};
+
+  const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+      if(!entry.isIntersecting) {
+        return;
+      }else {
+        entry.target.classList.add("appear");
+        appearOnScroll.unobserve(entry.target)
+      }
+    });
+  }, appearOptions)
+
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  });
